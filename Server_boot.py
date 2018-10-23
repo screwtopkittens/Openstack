3eb4292748fd66ff331d7a4337988eed07e0cfae
screwtopkittens <44369924+screwtopkittens@users.noreply.github.com>
2018-10-23 15:45:57 +0100

    Update Server_boot.py

--- Server_boot.py
+++ Server_boot.py
@@ -20,39 +20,6 @@ def BUILD_CUSTOM_SERVER():
   KEY_PAIR_NAME = raw_input("Enter the SSH key name: ")
   image = cs.images.get(IMAGE_VAR)
   flavor = cs.flavors.get(FLAVOR_VAR)
-  return;
-
-##selects task
-print " Welcome to Dans Amazing cloud server configuration tool please see the menu below and select an Action"
-print "1. Build Standard Server"
-print "2. Show built server details"
-print "3. delete server"
-print "4. show avalible images"
-print "5. show avalible flavors"
-action = raw_input("Enter a selection: ")
-
-if action == "1":
-  BUILD_CUSTOM_SERVER()
-  
-if action == "2":
-  print cs.servers.list()
-  
-if action == "3":
-  ##dELETES server##
-  server.delete()
-  
-if action == "4":
-  for img in imgs:
-    print img.name, "  -- ID:", img.id
-    
-if action == "5":
-  for flv in flvs:
-    print "Name:", flv.name
-    print "  ID:", flv.id
-    print "  RAM:", flv.ram
-    print "  Disk:", flv.disk
-    print "  VCPUs:", flv.vcpus
-
   ##Gets keypair to use with server##
   public_key = open(os.path.expanduser("~/.ssh/id_rsa.pub")).read()
   keypair = cs.keypairs.create(KEY_PAIR_NAME, public_key)
@@ -60,6 +27,55 @@ if action == "5":
   ##Builds Server##
   server = cs.servers.create(SERVER_NAME, image.id, flavor.id, key_name=keypair.name)
   pyrax.utils.wait_for_build(server, verbose=True)
+  MAIN_MENU()
+
+def LIST_SERVERS():
+  print cs.servers.list()
+  MAIN_MENU()
+
+def AVALIBLE_IMAGES():
+  for img in imgs:
+    print img.name, "  -- ID:", img.id
+  MAIN_MENU()
+
+def AVALIBLE_FLAVORS():
+  for flv in flvs:
+    print "Name:", flv.name
+    print "  ID:", flv.id
+    print "  RAM:", flv.ram
+    print "  Disk:", flv.disk
+    print "  VCPUs:", flv.vcpus
+  MAIN_MENU()
+  
+def MAIN_MENU():
+  ##selects task
+  print " Welcome to Dans Amazing cloud server configuration tool please see the menu below and select an Action"
+  print "1. Build Standard Server"
+  print "2. Show built server details"
+  print "3. delete server"
+  print "4. show avalible images"
+  print "5. show avalible flavors"
+  action = raw_input("Enter a selection: ")
+
+  if action == "1":
+    BUILD_CUSTOM_SERVER()
+  
+  if action == "2":
+    LIST_SERVERS()
+  
+  if action == "3":
+    ##dELETES server##
+    server.delete()
+  
+  if action == "4":
+    AVALIBLE_IMAGES()
+    
+  if action == "5":
+    AVALIBLE_FLAVORS()
+    
+    
+MAIN_MENU()
+
   
 ##obtains images list
 ##images = pyrax.images.list()
