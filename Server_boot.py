ce2110a81f46e96a7ccbf2345c5147a510fd7270
screwtopkittens <44369924+screwtopkittens@users.noreply.github.com>
2018-10-23 17:11:32 +0100

    Update Server_boot.py

--- Server_boot.py
+++ Server_boot.py
@@ -1,3 +1,5 @@
+####DANs Super script for booting servers Version 1
+####Version 1.0.0.a
 import pyrax
 import os
 
@@ -17,18 +19,75 @@ def BUILD_CUSTOM_SERVER():
   SERVER_NAME = raw_input("Enter the server name: ")
   IMAGE_VAR = raw_input("Enter the image uuid: ")
   FLAVOR_VAR = raw_input("Enter the flavor id: ")
-  KEY_PAIR_NAME = raw_input("Enter the SSH key name: ")
   image = cs.images.get(IMAGE_VAR)
   flavor = cs.flavors.get(FLAVOR_VAR)
   ##Gets keypair to use with server##
   public_key = open(os.path.expanduser("~/.ssh/id_rsa.pub")).read()
-  keypair = cs.keypairs.create(KEY_PAIR_NAME, public_key)
+  keypair = cs.keypairs.create(SERVER_NAME, public_key)
 
   ##Builds Server##
   server = cs.servers.create(SERVER_NAME, image.id, flavor.id, key_name=keypair.name)
   pyrax.utils.wait_for_build(server, verbose=True)
+  print server.addresses
   MAIN_MENU()
 
+def STANDARD_SERVER_BUILD():
+   ##This sets the image and flavour Varibles
+  SERVER_NAME = raw_input("Enter the server name: ")
+  IMAGE_STA = "9a153957-6b23-4c82-bd98-2529b6f0ef6b"
+  FLAVOR_STA = "general1-1"
+  
+  print " Please select the image you would like to build"
+  print "1. Ubuntu 18.04 LTS (Bionic Beaver)"
+  print "2. Ubuntu 16.04 LTS (Xenial Xerus)"
+  print "3. Ubuntu 14.04 LTS (Trusty Tahr)"
+  print "4. CentOS 7"
+  print "5. CentOS 6"
+  print "6. Debian 9 (Stretch)"
+  action_srv = raw_input("Enter a selection: ")
+ 
+  if action_srv == "1":
+    IMAGE_STA = "9a153957-6b23-4c82-bd98-2529b6f0ef6b"
+  if action_srv == "2":
+    IMAGE_STA = "907770dd-ddf4-4281-afda-f9d25125c1bd"
+  if action_srv == "3":
+    IMAGE_STA = "0bb1924b-14fc-4638-a5a7-46024e4fcc1b"
+  if action_srv == "4":
+    IMAGE_STA = "8990ceb6-eb12-4c4f-b602-63b8459e02f9"
+  if action_srv == "5":
+    IMAGE_STA = "cce201c0-33f8-4a59-847e-352d2658fa1c"
+  if action_srv == "6":
+    IMAGE_STA = "2d970ae8-c358-4284-a43f-47cfd5968b70"
+    
+  print " Please select the flavor you would like to build"
+  print "1. 1 GB General Purpose v1"
+  print "2. 2 GB General Purpose v1"
+  print "3. 4 GB General Purpose v1"
+  print "4. 8 GB General Purpose v1"
+  action_flv = raw_input("Enter a selection: ")
+   
+  if action_flv == "1":
+    FLAVOR_STA = "general1-1"
+  if action_flv == "2":
+    FLAVOR_STA = "general1-2"
+  if action_flv == "3":
+    FLAVOR_STA = "general1-4"
+  if action_flv == "4":
+    FLAVOR_STA = "general1-8"
+    
+  image = cs.images.get(IMAGE_STA)
+  flavor = cs.flavors.get(FLAVOR_STA)
+  
+  ##Gets keypair to use with server##
+  public_key = open(os.path.expanduser("~/.ssh/id_rsa.pub")).read()
+  keypair = cs.keypairs.create(SERVER_NAME, public_key)
+  
+  ##Builds Server##
+  server = cs.servers.create(SERVER_NAME, image.id, flavor.id, key_name=keypair.name)
+  pyrax.utils.wait_for_build(server, verbose=True)
+  print server.addresses
+  MAIN_MENU()
+  
 def LIST_SERVERS():
   print cs.servers.list()
   MAIN_MENU()
@@ -50,9 +109,9 @@ def AVALIBLE_FLAVORS():
 def MAIN_MENU():
   ##selects task
   print " Welcome to Dans Amazing cloud server configuration tool please see the menu below and select an Action"
-  print "1. Build Standard Server"
+  print "1. Build Custom Server"
   print "2. Show built server details"
-  print "3. delete server"
+  print "3. Build standard Server"
   print "4. show avalible images"
   print "5. show avalible flavors"
   action = raw_input("Enter a selection: ")
@@ -64,8 +123,7 @@ def MAIN_MENU():
     LIST_SERVERS()
   
   if action == "3":
-    ##dELETES server##
-    server.delete()
+    STANDARD_SERVER_BUILD()
   
   if action == "4":
     AVALIBLE_IMAGES()
@@ -76,15 +134,7 @@ def MAIN_MENU():
     
 MAIN_MENU()
 
-  
-##obtains images list
-##images = pyrax.images.list()
-    
-##Obtains Flavour list
-##flavor_list = cs.list_flavors()
 
 
-##displayS ASSOCIATED ADDRESSES WITH CREATED SERVER##
-#server.addresses
 
 
