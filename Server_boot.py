57f96354a8bab796af4e658e02c11f1fe5c1c887
screwtopkittens <44369924+screwtopkittens@users.noreply.github.com>
2018-10-23 14:59:36 +0100

    Update Server_boot.py

--- Server_boot.py
+++ Server_boot.py
@@ -9,6 +9,8 @@ pyrax.set_setting("identity_type", "rackspace")
 pyrax.set_default_region('IAD')
 pyrax.set_credentials(USER, API_KEY)
 cs = pyrax.cloudservers
+imgs = cs.images.list()
+flvs = cs.list_flavors()
 
 ##selects task
 print " Welcome to Dans Amazing cloud server configuration tool please see the menu below and select an Action"
@@ -19,13 +21,34 @@ print "4. show avalible images"
 print "5. show avalible flavors"
 action = raw_input("Enter a selection: ")
 
+if action == "1":
+  ##This sets the image and flavour Varibles
+  SERVER_NAME = raw_input("Enter the server name: ")
+  IMAGE_VAR = raw_input("Enter the image uuid: ")
+  FLAVOR_VAR = raw_input("Enter the flavor id: ")
+  image = cs.imgs.get(IMAGE_VAR)
+  flavor = cs.flvs.get(FLAVOR_VAR)
+  
+  ##Gets keypair to use with server##
+  public_key = open(os.path.expanduser("~/.ssh/id_rsa.pub")).read()
+  keypair = cs.keypairs.create("mykeypair", public_key)
+
+  ##Builds Server##
+  server = cs.servers.create(SERVER_NAME, image.id, flavor.id, key_name=keypair.name)
+  pyrax.utils.wait_for_build(server, verbose=True)
+  
+if action == "2":
+  print cs.servers.list()
+  
+if action == "3":
+  ##dELETES server##
+  server.delete()
+  
 if action == "4":
-  imgs = cs.images.list()
   for img in imgs:
     print img.name, "  -- ID:", img.id
     
 if action == "5":
-  flvs = cs.list_flavors()
   for flv in flvs:
     print "Name:", flv.name
     print "  ID:", flv.id
@@ -35,30 +58,12 @@ if action == "5":
 
 ##obtains images list
 ##images = pyrax.images.list()
-
-
     
 ##Obtains Flavour list
 ##flavor_list = cs.list_flavors()
 
 
-
-
-##This sets the image and flavour Varibles
-#image = pyrax.images.get('907770dd-ddf4-4281-afda-f9d25125c1bd')
-# flavor = cs.flavors.get('3')
-
-##Generates Keypair to use for authentication with server##
-#public_key = open(os.path.expanduser("~/.ssh/id_rsa.pub")).read()
-#keypair = cs.keypairs.create("mykeypair", public_key)
-
-##Builds Server##
-#server = cs.servers.create('test', image.id, flavor.id, key_name=keypair.name)
-#pyrax.utils.wait_for_build(server_dan, verbose=True)
-
-
 ##displayS ASSOCIATED ADDRESSES WITH CREATED SERVER##
 #server.addresses
 
-##dELETES server##
-#server.delete()
+
