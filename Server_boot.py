4ba37eea784b239c3b4f9db2657dfba2032841c2
screwtopkittens <44369924+screwtopkittens@users.noreply.github.com>
2018-10-23 10:44:38 +0100

    Create Server_boot.py

---
+++ Server_boot.py
@@ -0,0 +1,42 @@
+import pyrax
+import os
+
+USER = raw_input("Enter Your Username: ")
+API_KEY = raw_input("Enter Your API KEY: ")
+
+##Sets API Credentials 
+pyrax.set_setting("identity_type", "rackspace")
+pyrax.set_default_region('IAD')
+pyrax.set_credentials(USER, API_KEY)
+
+##obtains images list
+images = pyrax.images.list()
+
+##Obtains Flavour list
+flavor_list = cs.list_flavors()
+
+print " Welcome to Dans Amazing cloud server configuration tool please see the menu below and select an Action"
+print "1. Build Standard Server"
+print "2. Show built server details"
+print "3. delete server"
+action = raw_input("Enter a selection: ")
+
+
+##This sets the image and flavour Varibles
+#image = pyrax.images.get('907770dd-ddf4-4281-afda-f9d25125c1bd')
+# flavor = cs.flavors.get('3')
+
+##Generates Keypair to use for authentication with server##
+#public_key = open(os.path.expanduser("~/.ssh/id_rsa.pub")).read()
+#keypair = cs.keypairs.create("mykeypair", public_key)
+
+##Builds Server##
+#server = cs.servers.create('test', image.id, flavor.id, key_name=keypair.name)
+#pyrax.utils.wait_for_build(server_dan, verbose=True)
+
+
+##displayS ASSOCIATED ADDRESSES WITH CREATED SERVER##
+#server.addresses
+
+##dELETES server##
+#server.delete()
